e4250b609b03e14fccae81d78339167c99eb0301
Lusenko <lusenko.sasha.2015@gmail.com>
2023-03-31 11:14:29 +0300

    feat: add new components

---
+++ src/app/ggg/ggg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ggg',
+  templateUrl: './ggg.component.html',
+  styleUrls: ['./ggg.component.scss']
+})
+export class GggComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
